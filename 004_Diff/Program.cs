968ee82275af7aaeac3d40830796daf2af966db7
Anna Smirnova <sanna_i@mail.ru>
2023-03-06 14:33:33 +0300

    004_Diff

---
+++ 004_Diff/Program.cs
@@ -0,0 +1,7 @@
+﻿// Уяимся делить
+// int - целые (32- разрядное число со знаком); double - дроби (64-разрядное число со знаком)
+// string - просто строки, могут быть пустые
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
