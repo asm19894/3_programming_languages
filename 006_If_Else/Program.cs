5cf944ccc3291d7c383bb1f0ec782e6139215942
Anna Smirnova <sanna_i@mail.ru>
2023-03-06 15:03:35 +0300

    006_If_Else

---
+++ 006_If_Else/Program.cs
@@ -0,0 +1,15 @@
+﻿// username - повторяет в точности введенное имя, а username.ToLower - переводит все буквы в нижний регистр
+// if и else - ниже описан синтаксис этих комманд
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "Маша")
+{
+    Console.WriteLine("УРА, это же МАХА!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
