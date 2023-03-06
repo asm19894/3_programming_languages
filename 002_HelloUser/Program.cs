be966276a4a65673ff31de13d2d0071958dcbaa6
Anna Smirnova <sanna_i@mail.ru>
2023-03-06 14:06:39 +0300

    002_HelloUser

---
+++ 002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Новая команда ReadLine - для ее использования нужны данные, некий контейнер с переменными
+// Новая команда Write - вместо WriteLine - выводит все в одну строчку
+// Папки bin и obj - папки дурного тона, из надо сносить для гита
+Console.Write("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
